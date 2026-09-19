0a96277377ca5fbce03e79caab2045b6a52a344a
Riza Khan <khanriza@gmail.com>
2026-09-19 09:56:28 -0400

    Initial commit

---
+++ test/auto-scaling-group.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutoScalingGroup from '../lib/auto-scaling-group-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auto-scaling-group-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutoScalingGroup.AutoScalingGroupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
